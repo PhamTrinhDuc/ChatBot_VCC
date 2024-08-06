72eb4fe2ab8486253076c508d5b7392056f45f73
PhamTrinhDuc <duc78240@gmail.com>
2024-08-06 17:53:49 +0700

    configuration

--- configs/load_config.py
+++ configs/load_config.py
@@ -23,22 +23,25 @@ class LoadConfig:
         self.load_llm_config(app_config=app_config)
         self.load_chunk_config(app_config=app_config)
         self.load_retriver_config(app_config=app_config)
+        self.load_config_product(app_config=app_config)
 
     def load_directories(self, app_config):
         # Load parameters directories from load_config.yml file 
         self.persist_vector_directory = (
             app_config['directories']['persist_vector_directory']
         )
-        self.data_product_directory = (
-            app_config['directories']['data_product_directory']
+        self.csv_product_directory = (
+            app_config['directories']['csv_product_directory']
         )
-        self.database_product_directory = (
-            app_config['directories']['database_product_directory']
+        self.text_product_directory = (
+            app_config['directories']['text_product_directory']
         )
+
         
     def load_llm_config(self, app_config):
         # Load parameters llm from load_config.yml file 
         self.rag_model = app_config['llm_config']['rag_model']
+        self.chatchit_model = app_config['llm_config']['chatchit_model']
         self.temperature = app_config['llm_config']['temperature']
         self.max_token = app_config['llm_config']['max_token']
         
@@ -53,23 +56,36 @@ class LoadConfig:
         self.chunk_size = app_config['chunk_config']['chunk_size']
         self.chunk_overlap = app_config['chunk_config']['chunk_overlap']
 
+    def load_config_product(self, app_config):
+        self.id_2_name = app_config['product']['id_2_name']
+
     def load_embedding_model(self) -> FastEmbedEmbeddings:
         embedding_model = FastEmbedEmbeddings(model_name=self.embedding_model,)
                                             #   model_kwargs = {"device":"cpu"})
         return embedding_model
 
-    def load_groq_model(self) -> ChatGroq:
+    def load_rag_model(self) -> ChatGroq:
         """
         Load GROQ model using API
         """
-        langchain_groq = ChatGroq(
+        rag_model = ChatGroq(
                 model=self.rag_model,
                 api_key=os.getenv("GROQ_API_KEY"),
                 temperature=self.temperature,
                 max_tokens=self.max_token,
                 verbose=True,
             )
-        return langchain_groq
+        return rag_model
+    
+    def load_chatchit_model(self) -> ChatGroq:
+        chatchit_model = ChatGroq(
+            model=self.chatchit_model,
+                api_key=os.getenv("GROQ_API_KEY"),
+                temperature=self.temperature,
+                max_tokens=self.max_token,
+                verbose=True,
+            )
+        return chatchit_model
     
     def load_openai_model(self) -> ChatOpenAI:
         langchain_openai = ChatOpenAI(
