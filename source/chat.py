e5efbc2e46ae4d4f1d57e77d84ac446c224f8b8f
PhamTrinhDuc <duc78240@gmail.com>
2024-08-07 09:38:21 +0700

    chat model

--- source/chat.py
+++ source/chat.py
@@ -1,72 +1,82 @@
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferWindowMemory
 from source.retriever import get_context
 from configs.load_config import LoadConfig
 from source.utils.prompt import PROMPT_HISTORY, PROMPT_HEADER
 
 APP_CFG = LoadConfig()
 llm = APP_CFG.load_rag_model()
-memory = ConversationBufferMemory(memory_key="chat_history")
+memory = ConversationBufferWindowMemory(memory_key="chat_history", k=3)
 
 def get_history():
     history = memory.load_memory_variables({})
     return history['chat_history']
 
+
 def rewrite_query(query: str, history: str) -> str: 
+    """
+    Arg:
+        query: câu hỏi của người dùng
+        history: lịch sử của người dùng
+        Sử dụng LLM để viết lại câu hỏi của người dùng thành 1 câu mới.
+    Return:
+        trả về câu hỏi được viết lại.
+    """
     query_rewrite = llm.invoke(PROMPT_HISTORY.format(question=query, chat_history=history)).content
     return query_rewrite
 
-# def chat_with_history(query: str, history):
-#     context = get_context(query=query)
-#     response = None
-
-#     if context == "":
-
-#         template = f"""
-#         Bạn là 1 chuyên gia trong lĩnh vực trò chuyện, tâm sự với khách hàng tới mua sản phẩm bên bạn. 
-#         Bạn sẽ được cung cấp 1 câu hỏi của người dùng.
-#         Hãy trả lời họ và làm cho họ hài lòng. Bạn sẽ được thưởng 1000$ nếu bạn làm tốt. 
-#         Lưu ý: Khách hàng là người việt nên bạn chỉ được sử dụng tiếng việt
-
-#         Question: {query}
-
-#         Answer: """
-
-#         prompt = template.format(query=query)
-#         response = APP_CFG.load_chatchit_model().invoke(prompt).content
-        
-#         memory.chat_memory.add_user_message(prompt)
-#         memory.chat_memory.add_ai_message(response)
-#         history.append((prompt, response))
-
-#     else:
-#         history_conversation = get_history()
-#         query_rewrite = rewrite_query(query=query, history=history_conversation)
-#         prompt_final = PROMPT_HEADER.format(question=query_rewrite, context=context)
-
-#         response = llm.invoke(prompt_final).content
-
-#         memory.chat_memory.add_user_message(prompt_final)
-#         memory.chat_memory.add_ai_message(response)
-
-#         history.append((prompt_final, response))
-
-#     return "", history
 
 def chat_with_history(query: str, history):
-    history_conversation = get_history()
-    query_rewrite = rewrite_query(query=query, history=history_conversation)
-    context = get_context(query=query_rewrite)
-    print(context)
-    prompt_final = PROMPT_HEADER.format(question=query_rewrite, context=context)
+    context = get_context(query=query)
+    response = None
 
-    response = llm.invoke(prompt_final).content
+    if context == "":
 
-    memory.chat_memory.add_user_message(query)
-    memory.chat_memory.add_ai_message(response)
+        template = f'''
+        Hãy trò chuyện với khách hàng một cách thân thiện và tự nhiên. 
+        Trả lời các câu hỏi, chia sẻ thông tin hữu ích, và tham gia vào các cuộc trò chuyện đa dạng về nhiều chủ đề. 
+        Thích nghi với giọng điệu và phong cách của người dùng, đồng thời duy trì tính nhất quán và lịch sự.
+        Lưu ý: Khách hàng là người việt nên bạn chỉ được sử dụng tiếng việt
 
-    history.append((query, response))
+        Question: {query}
+
+        Answer: '''
+
+        prompt = template.format(query=query)
+        response = APP_CFG.load_chatchit_model().invoke(prompt).content
+        
+        memory.chat_memory.add_user_message(query)
+        memory.chat_memory.add_ai_message(response)
+        history.append((query, response))
+
+    else:
+        history_conversation = get_history()
+        query_rewrite = rewrite_query(query=query, history=history_conversation)
+        prompt_final = PROMPT_HEADER.format(question=query_rewrite, context=context)
+
+        response = llm.invoke(prompt_final).content
+
+        memory.chat_memory.add_user_message(query)
+        memory.chat_memory.add_ai_message(response)
+
+        history.append((query, response))
 
     return "", history
+
+# def chat_with_history(query: str, history):
+#     history_conversation = get_history()
+#     query_rewrite = rewrite_query(query=query, history=history_conversation)
+#     context = get_context(query=query_rewrite)
+#     print(context)
+#     prompt_final = PROMPT_HEADER.format(question=query_rewrite, context=context)
+
+#     response = llm.invoke(prompt_final).content
+
+#     memory.chat_memory.add_user_message(query)
+#     memory.chat_memory.add_ai_message(response)
+
+#     history.append((query, response))
+
+#     return "", history
 
 
 
